447c2cc1aa5cf67d0927db156dd33927e43d4866
solutionyogi <contact@solutionyogi.com>
2014-03-01 05:57:09 -0500

    First pass at Root element and a simple tag.

--- ZenXml.Core/ZenXmlObject.cs
+++ ZenXml.Core/ZenXmlObject.cs
@@ -1,46 +1,150 @@
 ﻿using System;
 using System.Dynamic;
 using System.IO;
+using System.Linq;
 using System.Xml.Linq;
+using NLog;
 
 namespace ZenXml.Core
 {
     public class ZenXmlObject : DynamicObject
     {
-        private ZenXmlObject()
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private readonly XDocument _document;
+
+        private readonly ZenXmlObject _parent;
+
+        private readonly StringComparison? _comparison;
+
+        private ZenXmlObject Parent
         {
+            get { return _parent; }
         }
 
-        public XDocument Document { get; private set; }
-
-        public static ZenXmlObject CreateFromXml(string xml)
+        public XDocument Document
         {
-            if(string.IsNullOrWhiteSpace(xml))
-                throw new ArgumentNullException("xml");
+            get
+            {
+                if(_document != null)
+                    return _document;
 
-            return CreateFromXDocument(XDocument.Parse(xml));
+                var currentParent = Parent;
+                var document = currentParent._document;
+                while(document == null)
+                {
+                    currentParent = currentParent.Parent;
+                    document =  currentParent._document;
+                }
+
+                return document;
+            }
         }
 
-        public static ZenXmlObject CreateFromFile(string xmlFilePath)
+        public StringComparison Comparison
         {
-            if(string.IsNullOrWhiteSpace(xmlFilePath))
-                throw new ArgumentNullException("xmlFilePath");
-            
-            if(! File.Exists(xmlFilePath))
-                throw new ArgumentException(string.Format("File specified by path {0} does not exist.", xmlFilePath));
+            get
+            {
+                if(_comparison.HasValue)
+                    return _comparison.Value;
 
-            return CreateFromXDocument(XDocument.Load(xmlFilePath));
+                var currentParent = Parent;
+                var comparison = currentParent._comparison;
+                while(!comparison.HasValue)
+                {
+                    currentParent = currentParent.Parent;
+                    comparison = currentParent._comparison;
+                }
+
+                return comparison.Value;
+            }
         }
 
-        private static ZenXmlObject CreateFromXDocument(XDocument document)
+        private bool IsRoot
+        {
+            get { return _document != null; }
+        }
+
+        private ZenXmlObject(ZenXmlObject parent)
+        {
+            _parent = parent;
+        }
+
+        protected ZenXmlObject(XDocument document, StringComparison comparison)
         {
             if(document == null)
                 throw new ArgumentNullException("document");
 
-            return new ZenXmlObject
-                   {
-                       Document = document
-                   };
+            _document = document;
+            _comparison = comparison;
+        
+        }
+
+        public static dynamic CreateFromXml(string xml)
+        {
+            return CreateFromXml(xml, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static dynamic CreateFromXml(string xml, StringComparison comparison)
+        {
+            if(string.IsNullOrWhiteSpace(xml))
+                throw new ArgumentNullException("xml");
+
+            return CreateFromXDocument(XDocument.Parse(xml), comparison);
+        }
+
+        public static dynamic CreateFromFile(string xmlFilePath)
+        {
+            return CreateFromFile(xmlFilePath, StringComparison.OrdinalIgnoreCase);
+        }
+        public static dynamic CreateFromFile(string xmlFilePath, StringComparison comparison)
+        {
+            if(string.IsNullOrWhiteSpace(xmlFilePath))
+                throw new ArgumentNullException("xmlFilePath");
+
+            if(! File.Exists(xmlFilePath))
+                throw new ArgumentException(string.Format("File specified by path {0} does not exist.", xmlFilePath));
+
+            return CreateFromXDocument(XDocument.Load(xmlFilePath), comparison);
+        }
+
+        public static dynamic CreateFromXDocument(XDocument document)
+        {
+            return CreateFromXDocument(document, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static dynamic CreateFromXDocument(XDocument document, StringComparison comparison)
+        {
+            if(document == null)
+                throw new ArgumentNullException("document");
+
+            return new ZenXmlObject(document, comparison);
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            Logger.Trace(string.Format("Binder.Name: {0}", binder.Name));
+
+            if(binder.Name.Equals("Root"))
+            {
+                Logger.Trace("Returning Root.");
+                result = new ZenXmlObject(this);
+                return true;
+            }
+
+            var element = Document.Elements().SingleOrDefault(x => x.Name.LocalName.Equals(binder.Name, Comparison));
+
+            if(element != null)
+            {
+                if(!element.HasElements && !element.HasAttributes)
+                {
+                    result = element.Value;
+                    return true;
+                }
+            }
+
+            result = null;
+            return true;
         }
     }
 }
---
+++ ZenXml.Test/MainTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NLog;
+using NLog.Config;
+using NLog.Targets;
+using Xunit;
+using ZenXml.Core;
+
+namespace ZenXml.Test
+{
+    public class MainTest
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public MainTest()
+        {
+            ConfigureNLog();
+        }
+
+        private static void ConfigureNLog()
+        {
+            var consoleTarget = new ConsoleTarget();
+            var loggingRule = new LoggingRule("*", LogLevel.Trace, consoleTarget);
+            var loggingConfiguration = new LoggingConfiguration();
+            loggingConfiguration.LoggingRules.Add(loggingRule);
+            LogManager.Configuration = loggingConfiguration;
+        }
+
+        private const string TestXml = @"<?xml version=""1.0"" encoding=""utf-8"" ?>
+
+        <Root>
+
+            <Item1>My Item 1</Item1>
+            
+        
+        </Root>
+";
+        [Fact]
+        public void TestRoot()
+        {
+            var zenXml = ZenXmlObject.CreateFromXml(TestXml, StringComparison.OrdinalIgnoreCase);
+            Logger.Info(zenXml.Root.GetType());
+            
+            Logger.Info((object) zenXml.Root.Item1);
+        }
+    }
+}
